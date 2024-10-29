c174c2903e01c9564287172bead550a469fa63d1
nrayamajhee <nishan@rayamajhee.com>
2024-10-29 16:56:51 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import Editor from "@monaco-editor/react";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { LaTeXJSComponent } from "https://cdn.jsdelivr.net/npm/latex.js/dist/latex.mjs";
+import { DOMAttributes, useState } from "react";
+
+customElements.define("latex-js", LaTeXJSComponent);
+type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      ["latex-js"]: CustomElement<LaTeXJSComponent>;
+    }
+  }
+}
+
+function App() {
+  const [text, setText] = useState("");
+  const handleUpdate = (value?: string) => {
+    if (value) {
+      setText(value);
+    }
+  };
+  return (
+    <div className="grid grid-cols-2 min-h-screen">
+      <Editor
+        height="100vh"
+        defaultLanguage="latex"
+        defaultValue={text}
+        onChange={handleUpdate}
+      />
+      <latex-js baseURL="https://cdn.jsdelivr.net/npm/latex.js/dist/">
+        {text}
+      </latex-js>
+    </div>
+  );
+}
+
+export default App;
