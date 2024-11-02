dfc01d6639d4ad682ef28a413a520c5db6510bfe
nrayamajhee <nishan@rayamajhee.com>
2024-11-02 12:21:26 -0700

    Setup github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
-import "katex/dist/katex.min.css";
-import Editor from "@monaco-editor/react";
-import TeX from "@matejmazur/react-katex";
+import Editor, { Monaco } from "@monaco-editor/react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { useState } from "react";
+import { markdown } from "./content";
 
 function App() {
-  const [text, setText] = useState("\\TeX\nE=mc^2");
-
+  const [text, setText] = useState(markdown);
   const handleUpdate = (value?: string) => {
     if (value) {
       setText(value);
     }
   };
+  function handleOnMount(_editor: any, monaco: Monaco) {
+    monaco.editor.addKeybindingRule({
+      keybinding: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyF,
+      command: null,
+    });
+    monaco.editor.addKeybindingRule({
+      keybinding: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyH,
+      command: null,
+    });
+  }
   return (
     <div className="grid grid-cols-2 min-h-screen">
       <Editor
         height="100vh"
-        defaultLanguage="latex"
+        defaultLanguage="markdown"
         defaultValue={text}
+        theme="vs-dark"
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
         onChange={handleUpdate}
+        onMount={handleOnMount}
       />
       <div className="flex flex-col">
-        {text.split("\n").map((t) => (
-          <TeX settings={{ output: "html" }}>{t}</TeX>
-        ))}
+        <Markdown remarkPlugins={[remarkGfm]} className="markdown">
+          {text}
+        </Markdown>
       </div>
     </div>
   );
---
+++ src/content.ts
@@ -0,0 +1,42 @@
+export const markdown = `
+Heading 1
+===========
+Heading 2
+------------
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+
+**bold**
+
+_italic_ 
+
+[Link](http://nishan.rayamajhee.com) 
+
+![Image](https://picsum.photos/200)
+
+> Blockquote 
+
+\`Inline code\` with backticks 
+
+* List
+* List
+* List
+
+---
+
+1. One
+2. Two
+3. Three
+
+- [x] Done
+- [ ] ToDo
+
+\`\`\`=rust
+fn main() {
+    println!("Some rust code")
+}
+\`\`\`
+`;
