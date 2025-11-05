1debaa679eec9ed83990058dc7af43110807c59e
nrayamajhee <nishan@rayamajhee.com>
2025-11-05 04:28:55 -0800

    Prepare for dnd support

--- web/app/pages/photos.tsx
+++ web/app/pages/photos.tsx
@@ -1,12 +1,13 @@
 import { getAuth } from "@clerk/react-router/server";
 import { BiSolidCloudUpload } from "react-icons/bi";
 import { get, postForm } from "~/utils/query";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Photo } from "~/schema";
 import Spinner from "~/ui/Spinner";
 import { FiCamera } from "react-icons/fi";
-import { Form, useLoaderData } from "react-router";
+import { Form, useActionData, useLoaderData } from "react-router";
 import type { Route } from "./+types/photos";
+import toast from "react-hot-toast";
 
 export async function loader(args: Route.LoaderArgs) {
   const { getToken } = await getAuth(args);
@@ -22,20 +23,63 @@ export async function action(args: Route.ActionArgs) {
     const formData = await args.request.formData();
     const file = formData.get("photo") as File;
     if (file.size > 1024 * 1024 * 5) {
-      throw new Error("File size must be less than 5MB");
+      return { message: "File size must be less than 5MB", type: "error" };
     }
-    await postForm("/photos", token, formData);
+    try {
+      const result = await postForm("/photos", token, formData);
+      return { type: "success", message: result };
+    } catch (e) {
+      return {
+        type: "error",
+        message: (e as Error)?.message ?? undefined,
+      };
+    }
   }
 }
 
 export default function Photos() {
+  const [dragging, setDragging] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const actionData = useActionData();
+  useEffect(() => {
+    if (actionData?.type === "error") {
+      toast.error(actionData?.message);
+    }
+    setDragging(false);
+    setUploading(false);
+  }, [actionData]);
   const { photos } = useLoaderData<typeof loader>();
-  const formRef = useRef<HTMLFormElement>(null);
-  const isCreating = false;
+  const handleDrop = (files: FileList) => {
+    if (inputRef.current && formRef.current) {
+      setUploading(true);
+      inputRef.current.files = files;
+      formRef.current.submit();
+    }
+  };
   return (
-    <div className="flex flex-col gap-8 relative min-h-[512px]">
-      <div className="hidden absolute bg-white/10 backdrop-blur-md w-full h-full top-0 left-0 rounded-3xl grid place-items-center ">
-        <BiSolidCloudUpload size={128} />
+    <div
+      className="flex flex-col gap-8 relative min-h-[512px] p-6 md:p-8 [& *]:pointer-events-none"
+      onDragEnter={() => setDragging(true)}
+      onDragOver={(e) => e.preventDefault()}
+      onDrop={(e) => {
+        e.preventDefault();
+        handleDrop(e.dataTransfer.files);
+      }}
+      onDragLeave={(event) => {
+        if (!event.currentTarget.contains(event.relatedTarget as Node))
+          setDragging(false);
+      }}
+    >
+      <div
+        className={`${dragging ? "grid" : "hidden"} absolute bg-white/10 backdrop-blur-md w-full h-full top-0 left-0 rounded-3xl place-items-center`}
+      >
+        {dragging && uploading ? (
+          <Spinner />
+        ) : (
+          <BiSolidCloudUpload size={128} />
+        )}
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1 items-stretch">
         {photos.map((photo: Photo) => (
@@ -57,7 +101,7 @@ export default function Photos() {
             htmlFor="picUpload"
             className="aspect-square min-h-full bg-zinc-700/50 hover:bg-zinc-700/80 active:bg-zinc-700/60 focus:bg-zinc-700/80 transition-colors outline-none flex items-center justify-center gap-2 p-4 cursor-pointer"
           >
-            {isCreating ? (
+            {!dragging && uploading ? (
               <Spinner />
             ) : (
               <>
--- web/app/ui/Spinner.tsx
+++ web/app/ui/Spinner.tsx
@@ -1,8 +1,13 @@
+const variants = {
+  small: "w-3 h-3",
+  large: "w-8 h-8",
+  default: "w-6 h-6",
+};
 type SpinnerProps = {
-  size?: "small";
+  size?: keyof typeof variants;
 };
 export default function Spinner({ size: variant }: SpinnerProps) {
-  const size = variant === "small" ? "w-3 h-3" : "w-6 h-6";
+  const size = variants[variant ?? "default"];
   return (
     <i
       className={
